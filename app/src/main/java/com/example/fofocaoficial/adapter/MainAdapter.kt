648777f4726e3860b9560b494a58d1142c50fe7d
uhlalari <a.mercadologa@gmail.com>
2022-07-19 18:34:28 -0300

    Upgrade no layout, alterando a imageView e descricoes de noticias para otimizacao e para facilitar visualizacao no geral

--- app/src/main/java/com/example/fofocaoficial/adapter/MainAdapter.kt
+++ app/src/main/java/com/example/fofocaoficial/adapter/MainAdapter.kt
@@ -1,9 +1,12 @@
 package com.example.fofocaoficial.adapter
 
+import android.icu.text.SimpleDateFormat
+import android.os.Build
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
@@ -12,6 +15,8 @@ import com.bumptech.glide.Glide
 import com.example.fofocaoficial.R
 import com.example.fofocaoficial.model.Article
 import kotlinx.android.synthetic.main.item_news.view.*
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 
 class MainAdapter : Adapter<MainAdapter.ArticleViewHolder>() {
@@ -37,14 +42,13 @@ class MainAdapter : Adapter<MainAdapter.ArticleViewHolder>() {
 
     override fun getItemCount(): Int = differ.currentList.size
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onBindViewHolder(holder: ArticleViewHolder, position: Int) {
         val article = differ.currentList[position]
         holder.itemView.apply {
             Glide.with(this).load(article.urlToImage).into(ivArticleImage)
             tvTitle.text = article.title
-            tvSource.text = article.author
-            tvDescription.text = article.description
-            tvPublishedAt.text = article.publishedAt
+            tvPublishedAt.text = getDateRelease(article.publishedAt)
 
             setOnClickListener {
                 onItemClickListener?.let { click ->
@@ -59,6 +63,22 @@ class MainAdapter : Adapter<MainAdapter.ArticleViewHolder>() {
         onItemClickListener = Listener
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun getDateRelease(date: String?) : String {
+        return if (date != null && date != "") {
+            val secondApiFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'")
+            val timestamp =
+                (SimpleDateFormat("yyyy-MM-dd").parse(date)?.time ?: 0) / 1000// timestamp in Long
+            val timestampAsDateString = DateTimeFormatter.ISO_INSTANT
+                .format(java.time.Instant.ofEpochSecond(timestamp))
+            val dateTime = LocalDate.parse(timestampAsDateString, secondApiFormat)
+
+            ""+dateTime.dayOfMonth+"/"+dateTime.monthValue+"/" +
+                    ""+dateTime.year
+        } else{
+            ""
+        }
+    }
 }
 
 /*após configurar o adapter, seguimos para modelagem da arquitetura mvp, criamos o presenter News presenter e search presenter, view home e view e favorite  */
